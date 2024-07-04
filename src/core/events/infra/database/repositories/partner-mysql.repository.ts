ab843c8a105c060987ba47e5b435319b5ffb1c32
Caio Lucas <caiolfsantoss@gmail.com>
2024-07-04 19:52:11 -0300

    Criando primeira implementação de um repositório

---
+++ src/core/events/infra/database/repositories/partner-mysql.repository.ts
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import { EntityManager } from "@mikro-orm/core/EntityManager";
+import { Partner, PartnerId } from "src/core/events/domain/entities/partner.entity";
+import { PartnerRepositoryInterface } from "src/core/events/domain/repositories/partner.repository.interface";
+
+export class PartnerMysqlRepository implements PartnerRepositoryInterface {
+
+    constructor(private entityManager: EntityManager){}
+
+    async add(entity: Partner): Promise<void> {
+        this.entityManager.persist(entity);
+    }
+
+    findById(id: string | PartnerId): Promise<Partner | null> {
+        return this.entityManager.findOneOrFail(Partner, {
+            id: typeof id === 'string' ? new PartnerId(id): id,
+        });
+    }
+
+    findAll(): Promise<Partner[]> {;
+        return this.entityManager.findAll(Partner, {})
+    }
+
+    async delete(entity: Partner): Promise<void> {
+        this.entityManager.remove(entity);
+    }
+    
+}
