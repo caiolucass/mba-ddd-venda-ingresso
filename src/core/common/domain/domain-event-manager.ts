9dc815b3a630843372002b3f746465f463741dd6
Caio Lucas <caiolfsantoss@gmail.com>
2024-07-24 20:34:49 -0300

    Criando o gerenciador de eventos

---
+++ src/core/common/domain/domain-event-manager.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { EventEmitter } from "stream";
+import { AggregateRoot } from "./aggregate.root";
+import EventEmitter2 from "eventemitter2";
+
+export class DomainEventManager {
+    eventEmitter: EventEmitter2
+
+    constructor(){
+        this.eventEmitter = new EventEmitter2({
+            wildcard: true,
+        });
+    }
+
+    register(event: string, handler: any){
+      this.eventEmitter.on(event, handler);
+    }
+
+    async publish(aggregateRoot: AggregateRoot){
+      for(const event of aggregateRoot.events){
+        const eventClassName = event.constructor.name;
+        await this.eventEmitter.emitAsync(eventClassName, event);
+      }
+    }
+}
